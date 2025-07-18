ef4ee56fee5b72efc06ea5f1a6f8085d48a034d3
JevanC <jevanchahal1@gmail.com>
2025-07-17 20:01:30 -0700

    reruns upon error

--- pages/3_Talk_to_AI.py
+++ pages/3_Talk_to_AI.py
@@ -6,20 +6,26 @@ from db.database import query_db
 import os
 from dotenv import load_dotenv
 import os
+import time
 
-if "agent" not in st.session_state:
-    st.session_state.agent = agent.create_agent()
+try:
+    if "agent" not in st.session_state:
+        st.session_state.agent = agent.create_agent()
 
-if "config" not in st.session_state:
-    st.session_state.config = {"configurable": {"thread_id": "Base Thread"}}
+    if "config" not in st.session_state:
+        st.session_state.config = {"configurable": {"thread_id": "Base Thread"}}
 
-question = st.text_area("What is your question")
-button = st.button("Submit")
+    question = st.text_area("What is your question")
+    button = st.button("Submit")
 
-if button and question:
-    response = st.session_state.agent.invoke(
-        {"messages": [{"role": "user", "content": question}]},
-        st.session_state.config  
-    )
+    if button and question:
+        response = st.session_state.agent.invoke(
+            {"messages": [{"role": "user", "content": question}]},
+            st.session_state.config  
+        )
 
-    st.markdown(dict(response['messages'][-1])['content'])
+        st.markdown(dict(response['messages'][-1])['content'])
+except Exception as e:
+    st.markdown("Loading in website")
+    time.sleep(5)
+    st.rerun()
